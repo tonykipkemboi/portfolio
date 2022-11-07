52e9a104bb926ba0602f0a812a3e74febec3ed55
Tony Kipkemboi <tonykip@seas.upenn.edu>
2022-11-07 18:02:14 -0500

    Update utils.py

--- my_app/utils.py
+++ my_app/utils.py
@@ -3,7 +3,7 @@ import os
 
 
 def load_css():
-    with open('style.css') as f:
+    with open('my_app/style.css') as f:
         st.markdown('<style>{}</style>'.format(f.read()),
                     unsafe_allow_html=True)
     st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
