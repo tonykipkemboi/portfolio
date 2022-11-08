d8aa5feea484ee48ba0c8d6ec78f9ea03524379b
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2022-11-08 09:59:52 -0500

    Capitalization

--- my_app/pages/2_☎️_Contact.py
+++ my_app/pages/2_☎️_Contact.py
@@ -17,13 +17,13 @@ icon_size = 20
 st_button('youtube', 'https://www.youtube.com/c/TonyKipkemboi',
           ' Subscribe to my YouTube channel', icon_size)
 st_button('hashnode', 'https://townee.hashnode.dev/',
-          ' Read my Blogs', icon_size)
+          ' Read my blogs', icon_size)
 st_button('twitter', 'https://twitter.com/_townee/',
           ' Follow me on Twitter', icon_size)
 st_button('linkedin', 'https://www.linkedin.com/in/tonykipkemboi/',
           ' Follow me on LinkedIn', icon_size)
 st_button('cup', 'https://www.buymeacoffee.com/tonykip',
-          ' Buy me a Coffee', icon_size)
+          ' Buy me a coffee', icon_size)
 
 # Credits to Chanin a.k.a. "The Data Professor" for the Linktree --> Streamlit version tutorial
 st.caption(
